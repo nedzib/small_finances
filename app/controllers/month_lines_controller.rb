22a65433f06ec9f743b5290fa5913bb9cb9ee689
Nedzib Sastoque <bsastoque@buk.co>
2023-01-30 16:25:49 -0500

    feat: you can delete lines

--- app/controllers/month_lines_controller.rb
+++ app/controllers/month_lines_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class MonthLinesController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
 
   def create
     @month_line = MonthLine.new(post_params)
@@ -13,7 +14,18 @@ class MonthLinesController < ApplicationController
     end
   end
 
+  def destroy
+    @month_line.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer, notice: "Post was successfully destroyed." }
+    end
+  end
+
   private
+    def set_post
+      @month_line = MonthLine.find(params[:id])
+    end
+
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:month_line).permit(:concept, :type_id, :value, :additional_info, :paid, :month_id)
